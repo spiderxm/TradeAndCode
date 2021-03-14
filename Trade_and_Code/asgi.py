ababb38a4fcb3fc28c5c0b110405bdbc62af8c66
spiderxm <mrigank.rkee@rediffmail.com>
2021-03-15 00:09:12 +0530

    Adding Google Sign-in

---
+++ Trade_and_Code/asgi.py
@@ -0,0 +1,16 @@
+"""
+ASGI config for Trade_and_Code project.
+
+It exposes the ASGI callable as a module-level variable named ``application``.
+
+For more information on this file, see
+https://docs.djangoproject.com/en/3.1/howto/deployment/asgi/
+"""
+
+import os
+
+from django.core.asgi import get_asgi_application
+
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'Trade_and_Code.settings')
+
+application = get_asgi_application()
