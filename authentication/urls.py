14c6296d176b3fefaafd32460de7a1eb8d989a42
spiderxm <mrigank.rkee@rediffmail.com>
2021-03-21 21:42:41 +0530

    Adding more pages and adding icons

--- authentication/urls.py
+++ authentication/urls.py
@@ -5,5 +5,8 @@ urlpatterns = [
     path('', home, name="home"),
     path('leaderboard/', leaderboard, name="leaderboard"),
     path('components/', components_screen, name="components"),
-
+    path('developers/', developers, name="developers"),
+    path('not-started/', notStarted, name="notStarted"),
+    path('question/', question, name="question"),
+    path('register/', register, name="register"),
 ]
--- authentication/views.py
+++ authentication/views.py
@@ -18,8 +18,23 @@ def leaderboard(request):
     })
 
 
-@login_required()
 def components_screen(request):
     return render(request, template_name="component_screen.html", context={
         "components": Components.objects.all()
     })
+
+
+def developers(request):
+    return render(request, template_name="developers.html", context={
+    })
+
+
+def notStarted(request):
+    return render(request, template_name="NotStarted.html")
+
+
+def question(request):
+    return render(request, template_name="question.html")
+
+def register(request):
+    return render(request, template_name="register.html")
